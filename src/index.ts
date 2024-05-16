6214f96cf941d650b9222dfad4926a7a15c06dd3
acmepy <hjgamarra@gmail.com>
2024-05-16 10:30:47 -0400

    quitando dependecia de fs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { convertToPem } from './lib/p12';
-import * as fs from 'fs';
+//import * as fs from 'fs';
 
 export const Name = (name: string) => `Hello ${name}`;
 
-export function getPemFromP12(certPath: string, password: string) {
+/*export function getPemFromP12(certPath: string, password: string) {
   const p12File = fs.readFileSync(certPath, { encoding: 'binary' });
   return convertToPem(p12File, password);
-}
+}*/
 
 export function getPemFromP12Buffer(p12base64: any, password: string) {
   return convertToPem(p12base64, password);
